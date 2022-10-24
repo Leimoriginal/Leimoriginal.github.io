fb1b9f7565f65f8d56d967673f78cdbd76b5ae05
Leimoriginal <90452158+Leimoriginal@users.noreply.github.com>
2022-10-24 10:55:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Procrastinate.</title>
-  <link rel="stylesheet" href="style.css">
-  <!-- ниже подключили стили для мобильных устройств: в конце урока сможете поделиться проектом в социальных сетях без страха, что на телефоне друга или подруги будет выглядеть некрасиво -->
-  <link rel="stylesheet" href="https://code.s3.yandex.net/web-code/adaptive.css">
-  <link rel="icon" type="image" href="https://code.s3.yandex.net/web-code/js-favicon.ico">
-</head>
-<body>
-  <div class="header">
-    <p class="logo">Procrastinate.</p>
-    <div class="button">Ещё идея</div>
-  </div>
+let phrases = [
+  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
+  { text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
+  { text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
+  { text: 'Юрий Дудь', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
+  { text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
+  { text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
+  { text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
+  { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
+  { text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
+  { text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' },
+  { text: 'посадить кактус, чтобы не грустить' , image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngmart.com%2Fru%2Fimage%2Ftag%2Fcactus&psig=AOvVaw0Fhu_I8By-3aw3ieoj9_aL&ust=1666683744413000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCMD8kfeu-PoCFQAAAAAdAAAAABAw'},
+];
 
-  <img class="image" src="https://code.s3.yandex.net/web-code/procrastinate/9.png">
+function getRandomElement(arr) {
+  let randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+}
 
-  <div class="advice">
-    <span>Не писать код, а</span>
-    <span class="phrase">что бы поделать?</span>
-    <img class="cursor" src="https://code.s3.yandex.net/web-code/cursor.gif">
-  </div>
+let button = document.querySelector('.button');
+let phrase = document.querySelector('.phrase');
+let advice = document.querySelector('.advice');
+let image = document.querySelector('.image');
 
-  <p class="footer">© 2020 Сделал Тим Бернерс-Ли</p>
+button.addEventListener('click', function () {
+  let randomElement = getRandomElement(phrases);
+  smoothly(phrase, 'textContent', randomElement.text);
+  smoothly(image, 'src', randomElement.image);
 
-  <script src="https://code.s3.yandex.net/web-code/smoothly.js"></script>
-  <script src="script.js"></script>
-</body>
-</html>
+  if (randomElement.text.length > 40) {
+    advice.style.fontSize = '33px';
+  } else {
+    advice.style.fontSize = '42px';
+  }
+});
 
-for (let i = 0; i <= 2; i = i + 1) {smoothly(phrase, 'textContent', phrases[i].text);
+for (let i = 0; i <= 2; i = i + 1) {
+  smoothly(phrase, 'textContent', phrases[i].text);
+  smoothly(image, 'src', phrases[i].image);
 }
